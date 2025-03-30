7aae96d6c4d6f5518f3d3f93ad765f5c350bce62
Amirali <atanabain@gmail.com>
2025-03-30 09:04:07 +0330

    Create the search part

--- home_module/urls.py
+++ home_module/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home_page")
+    path("", views.HomeView.as_view(), name="home_page"),
+    path("search", views.SearchView.as_view(), name="search_product_page"),
 ]
--- home_module/views.py
+++ home_module/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
+from product_module.models import Product, ProductCategory
+from django.db.models import Q
 
 class HomeView(TemplateView):
     template_name = "home_module/home.html"
+
+class SearchView(View):
+    def get(self, request, **kwargs):
+        input = self.request.GET.get("input")
+        active_products = Product.objects.filter(is_active=True)
+        product_result = active_products.filter(Q(name__icontains=input) | Q(brand__title__icontains=input))
+        category_result = ProductCategory.objects.filter(is_active=True, title__icontains=input)
+
+        context = {
+            "search":input,
+            "result":product_result,
+            "categories":category_result
+        }
+
+        return render(request, "product_module/search.html", context)

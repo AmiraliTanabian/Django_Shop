76c0c23c7b73f39776823d457516d518b674b3b1
Amirali <atanabain@gmail.com>
2025-04-01 15:59:30 +0330

    Moved user model to auth_module

--- auth_module/models.py
+++ auth_module/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    about_user = models.TextField(null=True, blank=True, verbose_name="درباره کاربر")
+    profile_image = models.ImageField(upload_to="Images/user_profile", verbose_name="آواتار کاربر",
+                                      null=True, blank=True)
+
+
+    def __str__(self):
+        if self.first_name != '' and self.last_name != '':
+            return self.get_full_name()
+
+        if self.username is not None:
+            return self.username
+        return self.email
+
+    class Meta:
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربر ها"
--- news_module/models.py
+++ news_module/models.py
@@ -3,24 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django_jalali.db.models import jDateTimeField
 from django.contrib.auth import get_user_model
 
-class User(AbstractUser):
-    about_user = models.TextField(null=True, blank=True, verbose_name="درباره کاربر")
-    profile_image = models.ImageField(upload_to="Images/user_profile", verbose_name="آواتار کاربر",
-                                      null=True, blank=True)
-
-
-    def __str__(self):
-        if self.first_name != '' and self.last_name != '':
-            return self.get_full_name()
-
-        if self.username is not None:
-            return self.username
-        return self.email
-
-    class Meta:
-        verbose_name = "کاربر"
-        verbose_name_plural = "کاربر ها"
-
 User_model = get_user_model()
 
 class ArticleTag(models.Model):

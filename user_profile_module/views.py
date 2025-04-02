70d6eb7d06749a0f5911795dcda71e6bc9d02436
Amirali <atanabain@gmail.com>
2025-04-03 02:56:35 +0330

    Create edit profile part

--- user_profile_module/views.py
+++ user_profile_module/views.py
@@ -5,6 +5,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.http import HttpRequest
 from .forms import EditProfileModelForm
+from django.contrib import messages
+
+
+from phonenumbers import parse, is_valid_number, number_type
+from phonenumbers.phonenumberutil import PhoneNumberType
 
 class ProfileDashboardPage(LoginRequiredMixin, TemplateView):
     login_url = reverse_lazy("login_page")
@@ -13,7 +18,48 @@ class ProfileDashboardPage(LoginRequiredMixin, TemplateView):
 class EditProfilePageView(LoginRequiredMixin, View):
     login_url = reverse_lazy("login_page")
     def get(self, request: HttpRequest):
-        edit_profile_form = EditProfileModelForm
+        edit_profile_form = EditProfileModelForm(instance=request.user)
         return render(request, "user_profile_module/edit_profile_page.html",
                       {"form":edit_profile_form})
+
+    def post(self, request: HttpRequest):
+        edit_profile_form = EditProfileModelForm(request.POST, request.FILES, instance=request.user)
+
+        if edit_profile_form.is_valid():
+            mobile = edit_profile_form.cleaned_data["phone_number"]
+
+            if mobile is not None:
+                try:
+                    parsed_number = parse(mobile, "IR")
+                    second_step_mobile_valid = is_valid_number(parsed_number) and number_type(mobile) == PhoneNumberType
+                except:
+                    second_step_mobile_valid = False
+
+                # valid phone number
+                if second_step_mobile_valid:
+                    edit_profile_form.save()
+
+                    messages.success(request, "اطلاعات شما ویرایش شد!")
+                    return render(request, "user_profile_module/edit_profile_page.html",
+                                  {"form": edit_profile_form})
+
+                # invalid phone number
+                else:
+                    messages.error(request, "تلفن همراه شما نادرست است!")
+                    return render(request, "user_profile_module/edit_profile_page.html",
+                                  {"form": edit_profile_form})
+
+            # dont have mobile on form
+            else:
+                edit_profile_form.save()
+                messages.success(request, "اطلاعات شما ویرایش شد!")
+                return render(request, "user_profile_module/edit_profile_page.html",
+                              {"form": edit_profile_form})
+
+
+        else:
+            return render(request, "user_profile_module/edit_profile_page.html",
+                          {"form": edit_profile_form})
+
+
 

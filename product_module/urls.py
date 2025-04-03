8d05e240a64fcf7816cb4ce0376091b38714d3c1
Amirali <atanabain@gmail.com>
2025-04-04 02:40:07 +0330

    Create product category page

--- product_module/urls.py
+++ product_module/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductPageView, ProductDetailView
+from .views import ProductPageView, ProductDetailView, ProductCategoryPageView
 
 
 urlpatterns = [
     path("", ProductPageView.as_view(), name="product_page"),
-    path("<int:pk>", ProductDetailView.as_view(), name="product_detail")
+    path("<int:pk>", ProductDetailView.as_view(), name="product_detail"),
+    path("category/<slug:slug>", ProductCategoryPageView.as_view(), name="product_category_page")
 ]
--- product_module/views.py
+++ product_module/views.py
@@ -9,10 +9,33 @@ class ProductPageView(ListView):
     context_object_name = "products"
     paginate_by = 5
 
-
 class ProductDetailView(DetailView):
     template_name = "product_module/product_detail.html"
     model = Product
+
+class ProductCategoryPageView(ListView):
+    template_name = "product_module/product_category_page.html"
+    model = Product
+    context_object_name = "products"
+    paginate_by = 5
+
+    def get_queryset(self):
+        slug = self.kwargs["slug"]
+        query = self.model.objects.filter(is_active=True)
+        ok_items = []
+        for item in query :
+            for cat in item.categories.all():
+                if cat.slug == slug :
+                    ok_items.append(item)
+                    break
+        return ok_items
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        cat_title = ProductCategory.objects.get(slug=self.kwargs["slug"]).title
+        context["cat_title"] = cat_title
+
+        return context
 
 def product_category_part_partial(request):
     cats = ProductCategory.objects.filter(is_active=True, parent=None).prefetch_related("childs")

06df0d13cfb1a11e6bbce5a603ffa1878a732a11
Amirali <atanabain@gmail.com>
2025-04-03 23:16:47 +0330

    add product for test + pagination on product_list

--- product_module/views.py
+++ product_module/views.py
@@ -6,7 +6,7 @@ class ProductPageView(ListView):
     template_name = "product_module/product_list.html"
     model = Product
     context_object_name = "products"
-    # paginate_by = 5
+    paginate_by = 5
 
 
 class ProductDetailView(DetailView):

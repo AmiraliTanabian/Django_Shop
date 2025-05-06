b86d02b38d651ef2996105108fefcf6602264c7c
Amirali <atanabain@gmail.com>
2025-05-06 23:39:30 +0330

    Create email validation for news letter

--- newsletter_module/static/newsletter_module/news_letter.js
+++ newsletter_module/static/newsletter_module/news_letter.js
@@ -1,18 +1,37 @@
+function email_validation(email) {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
+}
+
+
 function AddToNewsLetter() {
     console.log("salam")
     let email = $("#news_letter_email").val();
-    $.get("news-letter/add", {
-        "email": email,
-    }).then(re => {
-        console.log(re);
+
+    if (!email_validation(email)) {
         Swal.fire({
-            title: re.title,
-            text: re.text,
-            icon: re.icon,
+            title: 'ایمیل نامعتبر',
+            text: 'ایمیل شما معتبر نمیباشد!',
+            icon: 'error',
             showCancelButton: false,
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
         })
-    })
+    } else {
+        $.get("news-letter/add", {
+            "email": email,
+        }).then(re => {
+            console.log(re);
+            Swal.fire({
+                title: re.title,
+                text: re.text,
+                icon: re.icon,
+                showCancelButton: false,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+            })
+        })
+    }
+
 
 }

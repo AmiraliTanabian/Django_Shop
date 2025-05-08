f57d311fcb8117b0c8bee889600b84186620c31e
Amirali <atanabain@gmail.com>
2025-05-08 23:10:31 +0330

    Create article main page

--- admin_panel_module/urls.py
+++ admin_panel_module/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index_page, name="admin_index_page"),
+    path('articles', views.ArticlePageView.as_view(), name="admin_articles_page"),
 ]
--- admin_panel_module/views.py
+++ admin_panel_module/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpRequest
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from news_module.models import Article
 
 
 def index_page(request: HttpRequest):
     return render(request, "admin_panel_module/index.html")
+
+
+class ArticlePageView(ListView):
+    template_name = 'admin_panel_module/articles.html'
+    context_object_name = "articles"
+    paginate_by = 10
+    model = Article

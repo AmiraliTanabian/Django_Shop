6cd681670c3ec0f5b91ab78e9f350a9939827681
Amirali <atanabain@gmail.com>
2025-06-01 10:30:34 +0330

    Create the message detail page

--- admin_panel_module/static/admin_panel_module/assets/js/own_custom_js.js
+++ admin_panel_module/static/admin_panel_module/assets/js/own_custom_js.js
@@ -6,3 +6,26 @@ $('#date-picker').persianDatepicker({
         }
     }
 });
+
+
+function removeMsgAdmin(msgId) {
+    evenet.preventDefault();
+    Swal.fire({
+        title: 'تایید حذف',
+        text: 'آیا شما از حذف پیام مطمنئنید؟',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "حذف"
+    }).then((result) => {
+        confirmButtonText: "حذف"
+        if (result.isConfirmed) {
+            $.get("../remove-msg/", {
+                "msg_id": msgId,
+            }).then(re => {
+                console.log(re)
+            })
+        }
+    });
+}

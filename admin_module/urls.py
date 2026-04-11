4732aae7feb9e57554f4c14db148952428e6d8be
Amirali <atanabain@gmail.com>
2026-04-11 14:58:07 +0330

    Create setting main page

--- admin_module/urls.py
+++ admin_module/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.index, name="admin_home_page"),
+    path("settings/", views.setting_page, name="admin_setting_page"),
 ]
--- admin_module/views.py
+++ admin_module/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "admin_module/index.html")
+
+
+def setting_page(request):
+    return render(request, "admin_module/setting_main_page.html")

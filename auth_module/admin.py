95057b306d6383aa3870088e7afe9b6a01b43e8e
Amirali <atanabain@gmail.com>
2026-09-06 00:44:14 -0700

    refactor user model

--- auth_module/admin.py
+++ auth_module/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import TempUser
 
@@ -8,5 +10,26 @@ class TempUserAdmin(admin.ModelAdmin):
     list_display = ["username", "email", "date"]
 
 
-admin.site.register(get_user_model())
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"),
+         {"fields": ("first_name", "last_name", "email", "about_user", "profile_image", "phone_number", "address")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(get_user_model(), CustomUserAdmin)
 admin.site.register(TempUser, TempUserAdmin)

5c4113e150962a4d497a9096469577bd5e8f414d
Amirali <atanabain@gmail.com>
2026-09-07 19:33:09 -0700

    admin-panel: Create product brands list

--- admin_module/urls.py
+++ admin_module/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path("products/remove-product/<id>/", views.remove_product_ajax, name="admin_remove_product"),
     path("products/comments/<product_id>/", views.ProductCommentList.as_view(), name="admin_product_comments"),
     path("products/comment/<comment_id>/", views.ProductCommentDetail.as_view(), name="admin_product_comment_detail"),
+    path("products/brands/", views.ProductBrandList.as_view(), name="admin_product_brands"),
     path("products/tags/", views.AdminProductTagsList.as_view(), name="admin_product_tags_list"),
     path("products/remove-tag/<id>", views.remove_product_tag_ajax, name="admin_remove_product_tag"),
     path("products/set-active-tag/<id>/", views.set_product_tag_active, name="admin_remove_product_tag"),
--- admin_module/views.py
+++ admin_module/views.py
@@ -17,7 +17,7 @@ from django.views.generic import View, ListView
 
 from contact_module.models import ContactModel
 from news_module.models import Article, ArticleCategories, ArticleTag, ArticleComment
-from product_module.models import Product, ProductCategory, ProductTag, ProductComment
+from product_module.models import Product, ProductCategory, ProductTag, ProductComment, Brand
 from site_module.models import SiteSetting, SiteBanners, Slider
 from .forms import SettingEditForms, BannersEditForm, EditSliderForm, AdminContactForm, EditArticleForm, \
     AddArticleCatForm, AddArticleTagForm, EditCommentForms, EditProductForm, AddProductCatForm, AddProductTagForm, \
@@ -909,3 +909,19 @@ class ProductCommentDetail(PermissionRequiredMixin, View):
             "comment": current_comment,
             "score_range": score_range
         })
+
+
+class ProductBrandList(PermissionRequiredMixin, ListView):
+    paginate_by = 10
+    model = Brand
+    template_name = "admin_module/products/product_brands_list.html"
+    context_object_name = "brands"
+    permission_required = [
+        "product_module.view_brand",
+    ]
+    permission_denied_message = "شما دسترسی به دیدن لیست برند محصولات را ندارید"
+
+    def get_queryset(self):
+        query = super().get_queryset()
+        query = query.order_by("-id")
+        return query

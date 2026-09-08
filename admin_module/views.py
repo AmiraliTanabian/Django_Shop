f022963f20beb1e78fcd4750a607b51f4e495630
Amirali <atanabain@gmail.com>
2026-09-07 18:58:37 -0700

    admin-panel: Create the comment list for products

--- admin_module/views.py
+++ admin_module/views.py
@@ -17,7 +17,7 @@ from django.views.generic import View, ListView
 
 from contact_module.models import ContactModel
 from news_module.models import Article, ArticleCategories, ArticleTag, ArticleComment
-from product_module.models import Product, ProductCategory, ProductTag
+from product_module.models import Product, ProductCategory, ProductTag, ProductComment
 from site_module.models import SiteSetting, SiteBanners, Slider
 from .forms import SettingEditForms, BannersEditForm, EditSliderForm, AdminContactForm, EditArticleForm, \
     AddArticleCatForm, AddArticleTagForm, EditCommentForms, EditProductForm, AddProductCatForm, AddProductTagForm
@@ -849,3 +849,26 @@ def remove_product_ajax(request: HttpRequest, id):
             "msg": "خطایی رخ داد.",
             "icon": "error",
         })
+
+
+class ProductCommentList(PermissionRequiredMixin, ListView):
+    model = ProductComment
+    paginate_by = 10
+    context_object_name = "comments"
+    template_name = "admin_module/products/product_comments_list.html"
+    permission_required = [
+        "product_module.view_productcomment"
+    ]
+    permission_denied_message = "شما دسترسی برای مشاهده نظرات محصول را ندارید"
+
+    def get_queryset(self, *args, **kwargs):
+        query = super().get_queryset(*args, **kwargs)
+        current_product = get_object_or_404(Product, id=self.kwargs.get('product_id'))
+        query = query.filter(product=current_product).order_by("-id")
+        return query
+
+    def get_context_data(self, *args, **kwargs):
+        current_product = get_object_or_404(Product, id=self.kwargs.get('product_id'))
+        context = super().get_context_data(*args, **kwargs)
+        context["product"] = current_product
+        return context
